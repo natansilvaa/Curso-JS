b0a2cf38560bdddc6c8a6a478df7f811d66dd40e
natansilvaa <natan.picanco24@gmail.com>
2025-04-24 13:12:03 -0300

    terminei ox 010

--- aula-11/ex008.js
+++ aula-11/ex008.js
@@ -1,1 +1,6 @@
-var vel = 60.5
+var vel = 30.5
+console.log(`a velociadde do seu carro é de ${vel} km/h`)
+if (vel > 60){ //condição simples 
+    console.log(` você ultrapassoua a velociade permitida, MULTADO!!!`)
+}
+console.log(` dirija sempre usando sinto de segurança!`)
---
+++ aula-11/ex009.js
@@ -0,0 +1,7 @@
+var país = ('EUA')
+console.log(`seu país é o ${país}`)
+if (país == 'Brasil'){
+    console.log(` você é Brasileiro!`)
+}else{
+    console.log(`voce é estrangeiro!`)
+}
