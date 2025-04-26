1af6dfc4a47bf3c95f28de2b20a8e8410fff7a59
natansilvaa <natan.picanco24@gmail.com>
2025-04-26 13:04:44 -0300

    terminei o ex

--- aula-12ex/script.js
+++ aula-12ex/script.js
@@ -1,1 +1,15 @@
-alert('olá')
+
+var agora =  new Date()
+var hora= agora.getHours()
+var min = agora.getMinutes()
+var h=document.getElementById('h')
+var ft = document.getElementById('imgs')
+h.innerHTML=` Agora sao:${hora}`
+
+if(hora < 12){
+    ft.style.backgroundImage = "url('img/manha1.jpg')"
+}else if ( hora < 18){
+    ft.style.backgroundImage= "url('img/tarde.jpg')"
+}else{
+    ft.style.backgroundImage= "url('img/noite.jpg')"
+}
