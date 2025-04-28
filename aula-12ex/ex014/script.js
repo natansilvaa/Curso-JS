872810f2abd4b7bc63f1c534823a66939937ba15
natansilvaa <natan.picanco24@gmail.com>
2025-04-28 15:19:03 -0300

    terminei o ex014

--- aula-12ex/ex014/script.js
+++ aula-12ex/ex014/script.js
@@ -1,15 +1,18 @@
-
+function hora(){
 var agora =  new Date()
 var hora= agora.getHours()
-var min = agora.getMinutes()
 var h=document.getElementById('h')
 var ft = document.getElementById('imgs')
 h.innerHTML=` Agora sao:${hora}`
 
-if(hora < 12){
+if(hora <= 12){
     ft.style.backgroundImage = "url('img/manha1.jpg')"
-}else if ( hora < 18){
+    document.body.style.backgroundColor = '#e2cd9f'
+}else if ( hora <= 18){
     ft.style.backgroundImage= "url('img/tarde.jpg')"
+    document.body.style.backgroundColor ='#b9846f'
 }else{
     ft.style.backgroundImage= "url('img/noite.jpg')"
+    document.body.style.backgroundColor ='#515154'
+}
 }
