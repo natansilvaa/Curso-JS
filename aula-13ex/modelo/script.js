470a4438b006e66c17a3987fb83ce6fbbbe8da9e
natansilvaa <natan.picanco24@gmail.com>
2025-04-28 21:52:42 -0300

    js do ex

--- aula-13ex/modelo/script.js
+++ aula-13ex/modelo/script.js
@@ -1,10 +1,12 @@
 
 function enviar(){ 
     var idade = document.getElementById('idade')
-    var img = document.getElementById('imgs')
+    var imgs = document.getElementById('imgs')
     var sex = document.getElementById('sex')
     var res = document.getElementById('res')
     if(idade <=10 && sex == masculino){ 
+        res.innerHTML=(`criança de ${idade} anos do sexo ${sex}`)
+    }else if(idade <=10 && sex == feminimo){
         res.innerHTML=(`criança de ${idade} anos do sexo ${sex}`)
     }
  }
