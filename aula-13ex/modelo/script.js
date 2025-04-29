13e9d43dc032dc1251308ce9840c0c0f1547c034
natansilvaa <natan.picanco24@gmail.com>
2025-04-29 11:03:01 -0300

    fazendo o js

---
+++ aula-13ex/modelo/script.js
@@ -0,0 +1,14 @@
+function verificar(){
+    var data= new Date()
+    var ano= data.getFullYear()
+    var fano=document.getElementById('txtano')
+    var res = document.getElementById('res')
+
+    if (fano.value.length == 0 ||Number(fano.value) > ano){
+        window.alert('[ERRO] preencha os dados novamente!')
+    }else {
+      var fsex = document.getElementsByName('radsex')
+      var idade = ano - Number(fano.value)
+      res.innerHTML=(`vc tem ${idade}`)
+    }
+}
