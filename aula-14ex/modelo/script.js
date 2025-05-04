960650312894c8413123bb74db296d90c962c790
natansilvaa <natan.picanco24@gmail.com>
2025-05-04 12:48:58 -0300

    terminei o ex016

--- aula-14ex/modelo/script.js
+++ aula-14ex/modelo/script.js
@@ -1,7 +1,30 @@
-var inicio = document.getElementById("inicio")
-var fim = document.getElementById("fim")
-var passo= document.getElementById("pas")
+function contar() {
+    var inicio = document.getElementById("inicio").value
+    var fim = document.getElementById("fim").value
+    var passo = document.getElementById("pas").value
+    var res = document.getElementById("res")
 
-function contar(){
-    
+    // Conversão para números
+    inicio = Number(inicio)
+    fim = Number(fim)
+    passo = Number(passo)
+
+    if (isNaN(inicio) || isNaN(fim) || isNaN(passo) || passo <= 0) {
+        alert('Preencha os campos corretamente e use um passo maior que zero!')
+    } else {
+        res.innerHTML = "Contando: <br>"
+
+        if (inicio < fim) {
+            // Contagem crescente
+            for (let c = inicio; c <= fim; c += passo) {
+                res.innerHTML += `${c} 👉 `
+            }
+        } else {
+            // Contagem regressiva
+            for (let c = inicio; c >= fim; c -= passo) {
+                res.innerHTML += `${c} 👉 `
+            }
+        }
+        res.innerHTML += "🏁"
+    }
 }
