5ff183325a800703905ea7617fe0a9a50761ba25
natansilvaa <natan.picanco24@gmail.com>
2025-05-06 22:56:32 -0300

    123

--- aula-14ex/ex17/script.js
+++ aula-14ex/ex17/script.js
@@ -1,4 +1,7 @@
 function tabuada(){
     let num=document.getElementById("txtn")
     let yab= document.getElementById('soltab')
+    if(num.value.length ==0){
+        alert('preencha os dados')
+    }
 }
