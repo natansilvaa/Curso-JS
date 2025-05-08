89131a7a334cd92b8386729dcf137eef6742ade9
natansilvaa <natan.picanco24@gmail.com>
2025-05-08 11:01:56 -0300

    terminei o ex 17

--- aula-14ex/ex17/script.js
+++ aula-14ex/ex17/script.js
@@ -1,7 +1,17 @@
 function tabuada(){
     let num=document.getElementById("txtn")
-    let yab= document.getElementById('soltab')
+    let tab= document.getElementById('seltab')
     if(num.value.length ==0){
         alert('preencha os dados')
+    }else{
+        let n = Number(num.value)
+        let c = 1
+        while(c <=10){
+            let item= document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            tab.appendChild(item)
+            c++
+        }
     }
+
 }
